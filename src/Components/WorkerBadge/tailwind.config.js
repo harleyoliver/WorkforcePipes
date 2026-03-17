76d33383f7fd0f4f07147658042557c4b2b4df37
Harley Oliver <hello@heronamedharley.com>
2026-03-17 17:09:21 +1100

    Remove PCF code for proper structuring

--- src/Components/WorkerBadge/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./SampleControl/*.{ts,tsx,xml}",
-    "./SampleControl/**/*.{ts,tsx,xml}"
-  ],
-  theme: { extend: {} },
-  plugins: [],
-}
